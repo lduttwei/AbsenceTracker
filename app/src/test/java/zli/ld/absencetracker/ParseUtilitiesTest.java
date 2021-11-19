aefef5d3a2b6aa09c3f151e637dedb9719b31e98
Linus Duttweiler <lindut@az.zli.ch>
2021-11-19 13:58:40 +0100

    Unit Tests

--- app/src/test/java/zli/ld/absencetracker/ParseUtilitiesTest.java
+++ app/src/test/java/zli/ld/absencetracker/ParseUtilitiesTest.java
@@ -19,4 +19,20 @@ public class ParseUtilitiesTest {
         String date = "Kein datum";
         assert !ParseUtilities.verifyDate(date);
     }
+
+    @Test
+    public void verifyEmailCorrect(){
+        String email = "linus@mail.com";
+        assert ParseUtilities.verifyEmail(email);
+        email = "linus@mail.bbw.com";
+        assert ParseUtilities.verifyEmail(email);
+    }
+
+    @Test
+    public void verifyEmailFalse(){
+        String email = "linus@m@ail.com";
+        assert !ParseUtilities.verifyEmail(email);
+        email = "linus@mail.";
+        assert !ParseUtilities.verifyEmail(email);
+    }
 }
